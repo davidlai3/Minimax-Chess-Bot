cc54c3bc42b8d291c3919d626e542d00901965f9
nakibabedin <nabedin30@stuy.edu>
2024-08-12 19:53:22 -0400

    add new code structure

---
+++ Pawn.h
@@ -0,0 +1,18 @@
+#ifndef PAWN_H
+#define PAWN_H
+
+#include "include/Piece.h"
+
+class Pawn : public Piece {
+
+    public:
+        Pawn(pos startingPosition);
+
+    private:
+
+
+
+};
+
+
+#endif
--- include/Piece.h
+++ include/Piece.h
@@ -5,12 +5,21 @@
 #define pos std::pair<int, int>
 
 class Piece {
-
 	public:
 
+		enum Color {
+			WHITE,
+			BLACK
+		};
+
 		virtual std::vector<pos> getMoves() = 0;
-
-
+		void setNewPosition(pos newPosition);
+	
+	private:
+		Color color;
+		pos currentPosition;
+		
+		
 };
 
 #endif
